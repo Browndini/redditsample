658d813515bea424a3424e4cfa7e05fac3cee873
Browndini <dt.kylebrown@gmail.com>
2018-05-13 05:23:06 -0700

    tidy up comments

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 * `npm i`
 * `npm start`
+* Go to `localhost:3000/{reddit-page}`
--- app.js
+++ app.js
@@ -3,44 +3,37 @@ var express = require("express");
 let https = require("https");
 let _ = require("lodash");
 var moment = require("moment");
-var path = require("path");
-
-var indexRouter = require("./routes/index");
 
 var app = express();
 
-// view engine setup
-// app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static("public"));
 
-app.use("/:search/:next?", function(req, res) {
-  const search = req.params.search;
+app.use("/:search?/:next?", function(req, res) {
+  const search = req.params.search ? req.params.search : "all";
   const after =
     req.params.next !== undefined ? `?after=${req.params.next}` : "";
-  console.log(after);
   let data = "";
+
   https.get(`https://www.reddit.com/r/${search}.json${after}`, response => {
     response.on("data", d => {
       data += d;
     });
-    response.on("error", e => {
-      console.log("errrrror");
-    });
+
     response.on("end", () => {
-      // const theData = JSON.parse(data);
-      console.log("ddd ", JSON.parse(data).data.children);
-      res.render("index", {
-        moment: moment,
-        url: req.baseUrl,
-        search: req.params.search,
-        data: _.get(JSON.parse(data), "data", ""),
-        message: "Hello there!"
-      });
+      if (!JSON.parse(data).error) {
+        res.render("index", {
+          moment: moment,
+          url: req.baseUrl,
+          search: search,
+          data: _.get(JSON.parse(data), "data", "")
+        });
+      } else {
+        res.render("empty", {});
+      }
     });
   });
 });
